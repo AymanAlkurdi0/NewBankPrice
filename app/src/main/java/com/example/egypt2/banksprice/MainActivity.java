5ac0be0af108f7cbe9dc8722e76c8bec955b66b9
aymanalkurdi0 <aymanalkurdi0@gmail.com>
2017-09-28 18:25:17 +0200

    Edit the custom Header (action bar ) {icon,color,name,location} Enable each of Menu and Refresh Button

--- app/src/main/java/com/example/egypt2/banksprice/MainActivity.java
+++ app/src/main/java/com/example/egypt2/banksprice/MainActivity.java
@@ -1,30 +1,15 @@
 package com.example.egypt2.banksprice;
 
 import android.app.Fragment;
-import android.app.NotificationManager;
-import android.app.ProgressDialog;
-import android.app.job.JobInfo;
-import android.app.job.JobScheduler;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.NotificationCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
@@ -33,41 +18,28 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
-
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TextView;
-
 import com.example.egypt2.banksprice.myClass.AyFragment_cur_activity;
 import com.example.egypt2.banksprice.myClass.AyINFO_Drawer_layout;
-import com.example.egypt2.banksprice.myClass.AyNotification;
 import com.example.egypt2.banksprice.myClass.AyPop;
 import com.example.egypt2.banksprice.myClass.AyProgressDialog;
 import com.example.egypt2.banksprice.myClass.AySharedPreferences;
 import com.example.egypt2.banksprice.myClass.AyUiCuSelected;
 import com.example.egypt2.banksprice.myClass.DrawerListAdapter;
-import com.example.egypt2.banksprice.myClass.ayAsyncTask;
-import com.example.egypt2.banksprice.myClass.ayJobSchedule;
 import com.example.egypt2.banksprice.myClass.ayUI;
-
 import java.util.ArrayList;
-
 import jp.co.recruit_lifestyle.android.widget.WaveSwipeRefreshLayout;
 
 public class MainActivity extends AppCompatActivity {
+    boolean mSlideState;
     AyPop ayPop;
     android.app.FragmentManager manager = getFragmentManager();
     TextView ayLastUpdate,Curency_name;
     ListView ayListView;
     ImageView Curency_image;
     RelativeLayout RelativeLayout_DrawerLayout;
-    DrawerLayout DrawerLayout;
+    DrawerLayout mDrawerLayout;
     WaveSwipeRefreshLayout mWaveSwipeRefreshLayout;
     SharedPreferences sharedPreferences;
-    String URL ="http://aymanalkurdi002-001-site1.gtempurl.com/Price.asmx/Get_value_by_JSON";
     int JobNumber =123;
     LinearLayout header_linearLayout;
     @Override
@@ -88,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         menu_header_open_menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // ...
+                clickEventSlide();
             }
         });
 
@@ -100,16 +72,17 @@ public class MainActivity extends AppCompatActivity {
                 ayPop.show(manager,null);
             }
         });
-        ImageButton menu_header_photo_camera = (ImageButton) customView.findViewById(R.id.menu_header_photo_camera);
-        menu_header_photo_camera.setOnClickListener(new View.OnClickListener() {
+        ImageButton menu_header_currency_refresh = (ImageButton) customView.findViewById(R.id.menu_header_currency_refresh);
+        menu_header_currency_refresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // ...
+                //send the BroadCast
+                refresh_price_boradCast();
             }
         });
 
         //**************************************************************
-
+        mSlideState=false;
         ayLastUpdate = (TextView) findViewById(R.id.ayLastUpdate);
         ayListView = (ListView) findViewById(R.id.ayListView);
         Curency_name = (TextView) findViewById(R.id.header_cruncey_name);
@@ -140,48 +113,11 @@ public class MainActivity extends AppCompatActivity {
         mWaveSwipeRefreshLayout.setOnRefreshListener(new WaveSwipeRefreshLayout.OnRefreshListener() {
             @Override public void onRefresh() {
                 //send the BroadCast
-                Intent i = new Intent();
-                i.setAction("android.intent.data.complete");
-                sendBroadcast(i);
+                refresh_price_boradCast();
             }
         });
 //*********************************************************************
-       //************************************************************************************************************************************************************************
-        //set drawer tools
-        //conecet with UI
 
-         DrawerLayout = (DrawerLayout)findViewById(R.id.DrawerLayout);//create this for cancel the next :)
-        RelativeLayout_DrawerLayout =(RelativeLayout)findViewById(R.id.RelativeLayout_DrawerLayout); //to be able to cancel it
-
-        //set the listView for the ListView in RelativeLayout_DrawerLayout
-        ListView ListView_Drawer_Layout=(ListView)findViewById(R.id.ListView_Drawer_Layout);
-        //create a AyINFO_Drawer_layout and put on ArrayList
-        AyINFO_Drawer_layout drawerListAdapter =new AyINFO_Drawer_layout("Currency",R.drawable.currency); // set what you want for being showing in Drawer list (link for new page)
-        ArrayList<AyINFO_Drawer_layout> myArray = new ArrayList<AyINFO_Drawer_layout>();
-        myArray.add(drawerListAdapter);
-        //pass and create the adapter and ArrayList to class
-        DrawerListAdapter drawerListAdapter1 = new DrawerListAdapter(this,myArray);
-        //set the adapter to listView
-        ListView_Drawer_Layout.setAdapter(drawerListAdapter1);
-
-
-        //when clicked on item
-        ListView_Drawer_Layout.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        //call the function in main activity
-                selectItemFromDrawer(position);
-
-            }
-        });
-
-
-
-
-
-
-
-        //************************************************************************************************************************************************
         // two Solution
 
 
@@ -211,22 +147,42 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-
 */
 
+        //************************************************************************************************************************************************************************
+        //set drawer tools
+        //conecet with UI
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.DrawerLayout);//create this for cancel the next :)
+        RelativeLayout_DrawerLayout =(RelativeLayout)findViewById(R.id.RelativeLayout_DrawerLayout); //to be able to cancel it
+
+        //set the listView for the ListView in RelativeLayout_DrawerLayout
+        ListView ListView_Drawer_Layout=(ListView)findViewById(R.id.ListView_Drawer_Layout);
+        //create a AyINFO_Drawer_layout and put on ArrayList
+        AyINFO_Drawer_layout drawerListAdapter =new AyINFO_Drawer_layout("Currency",R.drawable.currency); // set what you want for being showing in Drawer list (link for new page)
+        ArrayList<AyINFO_Drawer_layout> myArray = new ArrayList<AyINFO_Drawer_layout>();
+        myArray.add(drawerListAdapter);
+        //pass and create the adapter and ArrayList to class
+        DrawerListAdapter drawerListAdapter1 = new DrawerListAdapter(this,myArray);
+        //set the adapter to listView
+        ListView_Drawer_Layout.setAdapter(drawerListAdapter1);
 
 
+        //when clicked on item
+        ListView_Drawer_Layout.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //call the function in main activity
+                selectItemFromDrawer(position);
+
+            }
+        });
 
 
-
-
+        //************************************************************************************************************************************************
 
 
     }
-
-
 
 
     //function for when select item from drawer_layout
@@ -237,9 +193,23 @@ public class MainActivity extends AppCompatActivity {
         getFragmentManager().beginTransaction()
                 .replace(R.id.main_relative_layout, fragment)
                 .commit();
+
 //to close RelativeLayout
-        DrawerLayout.closeDrawer(RelativeLayout_DrawerLayout);
+        mDrawerLayout.closeDrawer(RelativeLayout_DrawerLayout);
     }
+
+
+
+
+
+    public void clickEventSlide( ){
+        mSlideState = mDrawerLayout.isDrawerOpen(Gravity.START);
+        if(mSlideState){
+            mDrawerLayout.closeDrawer(Gravity.START);
+        }else{
+            mDrawerLayout.openDrawer(Gravity.START);
+        }}
+
 
     /*
 //Menu
@@ -290,6 +260,9 @@ public class MainActivity extends AppCompatActivity {
     }
 */
 //End Menu Code
+
+
+
     //method to can dismissPop from any Class
     public void dismissPop(){
         ayPop.dismiss();
@@ -306,6 +279,16 @@ public class MainActivity extends AppCompatActivity {
 
         ayUI.Sort_Update();
     }
+    public void refresh_price_boradCast() {
+        //send the BroadCast
+        Intent i = new Intent();
+        i.setAction("android.intent.data.complete");
+        sendBroadcast(i);
+    }
+
+
+
+
 
 
 
